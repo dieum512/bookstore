9d13c3f9fea34e3ec1bc25b83221cdcfad8692c6
dieum512 <akonkwadieum222@gmail.com>
2023-04-08 01:28:30 +0200

    Create the Navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <>
+            <nav>
+                <h1>Bookstore CMS</h1>
+                <ul>
+                    <li><Link to="/">Books</Link></li>
+                    <li><Link to="/categories">Categories</Link></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Nav;
