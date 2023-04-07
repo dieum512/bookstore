1e707a9fb9ec32660b6b114aac1157f25cc4a7be
dieum512 <akonkwadieum222@gmail.com>
2023-04-08 01:32:37 +0200

    Update the App.js page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import DisplayBooks from "./components/books/DisplayBooks";
+import Categories from "./components/Categories";
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<DisplayBooks />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
