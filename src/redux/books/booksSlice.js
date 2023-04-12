3d025c5b0f33996b7f87c7ffcf880630e807b8ff
dieum512 <akonkwadieum222@gmail.com>
2023-04-13 00:41:35 +0200

    Define a slice of state for books

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
+
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books.filter((book) => book.item_id !== action.payload.id);
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+
+export default bookSlice.reducer;
