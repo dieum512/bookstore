9eb1565610933678092f7e0f84b5ff0629cf1da5
dieum512 <akonkwadieum222@gmail.com>
2023-04-16 00:29:12 +0200

    Update the file

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -1,10 +1,51 @@
-const AddBook = () => (
-  <form>
-    <h2>ADD NEW BOOK</h2>
-    <input id="title" type="text" name="Add" placeholder="Book title" />
-    <input id="author" type="text" name="Add" placeholder="Category" />
-    <button type="submit">Add</button>
-  </form>
-);
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/booksSlice';
+
+const AddBook = () => {
+  const dispatch = useDispatch;
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      item_id: Date.now(),
+      title,
+      author,
+      category: 'Nonfiction',
+    };
+    dispatch(addBook(newBook));
+    setAuthor('');
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>ADD NEW BOOK</h2>
+      <label htmlFor="booksinput">
+        <input
+          id="title"
+          type="text"
+          name="Add"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Book title"
+        />
+      </label>
+      <label htmlFor="authorInput">
+        <input
+          id="author"
+          type="text"
+          name="Add"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Author"
+        />
+      </label>
+      <button type="submit">Add</button>
+    </form>
+  );
+};
 
 export default AddBook;
