88a73f02805a6b9e51b55cb4e785744c2263269a
dieum512 <akonkwadieum222@gmail.com>
2023-04-16 00:25:50 +0200

    Add the remove action and the category to the books

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ title, author }) => (
+const Book = ({
+  category, title, author, remove,
+}) => (
   <>
+    <p>{category}</p>
     <h2>{title}</h2>
     <p>{author}</p>
-    <button type="button">Delete</button>
+    <button type="button" onClick={remove}>Delete</button>
   </>
 );
 
 Book.propTypes = {
+  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  remove: PropTypes.func.isRequired,
 };
 
 export default Book;
