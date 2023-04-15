a55df89854f0e97a526bed104b5d9959e0b92ad6
dieum512 <akonkwadieum222@gmail.com>
2023-04-16 00:24:30 +0200

    Add the remove action

--- src/components/books/DisplayBooks.js
+++ src/components/books/DisplayBooks.js
@@ -1,36 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/booksSlice';
 import AddBook from './AddBook';
 import Book from './Books';
 
 const DisplayBooks = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Book 1',
-      author: 'Dieum',
-    },
-    {
-      id: 2,
-      title: 'Book 2',
-      author: 'Akonkwa',
-    },
-    {
-      id: 3,
-      title: 'Book 3',
-      author: 'Lwabag',
-    },
-    {
-      id: 4,
-      title: 'Book 4',
-      author: 'Kuji',
-    },
-  ];
+  const books = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
+
+  const handleRemove = (id) => {
+    dispatch(removeBook(id));
+  };
 
   return (
     <>
       <ul>
         {books.map((book) => (
-          <li key={book.id}>
-            <Book title={book.title} author={book.author} />
+          <li key={book.item_id}>
+            <Book
+              category={book.category}
+              title={book.title}
+              author={book.author}
+              remove={() => handleRemove(book.item_id)}
+            />
           </li>
         ))}
       </ul>
