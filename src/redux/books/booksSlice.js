e9966540e40fc8e945ff2f0dfdbfd5e5085f136d
dieum512 <akonkwadieum222@gmail.com>
2023-04-20 22:05:39 +0200

    Configure the API

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,26 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const GET_POST_BOOKS_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/jFgFPX1ePQ3LgYOp0Krp/books';
+const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  const response = await axios.get(GET_POST_BOOKS_URL);
+  return response.data;
+});
+
+const addNewBook = createAsyncThunk('books/addNewBook', async (book) => {
+  await axios.post(GET_POST_BOOKS_URL, book);
+});
+
+const deleteBook = createAsyncThunk('books/deleteBook', async (id) => {
+  const DELETE_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/jFgFPX1ePQ3LgYOp0Krp/books/${id}`;
+  await axios.delete(DELETE_URL, id);
+});
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
 
 };
 
@@ -37,8 +34,27 @@ const bookSlice = createSlice({
       books: state.books.filter((book) => book.item_id !== action.payload),
     }),
   },
+  extraReducers(builder) {
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      const receivedData = action.payload;
+      const items = Object.keys(receivedData);
+      const data = [];
+      items.forEach((item) => {
+        receivedData[item][0].item_id = item;
+        data.push(receivedData[item][0]);
+      });
+      return {
+        ...state,
+        books: data,
+      };
+    });
+  },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
+const { addBook, removeBook } = bookSlice.actions;
+
+export {
+  addBook, removeBook, fetchBooks, addNewBook, deleteBook,
+};
 
 export default bookSlice.reducer;
